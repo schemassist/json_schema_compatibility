1068a386cfe5c68b593796387d2aae8121c2de37
Tyler Ewing <tewing10@gmail.com>
2024-08-19 17:19:17 -0500

    feat: generate gem template with `bundle gem`

---
+++ lib/json_schema_compatibility.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "json_schema_compatibility/version"
+
+module JsonSchemaCompatibility
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/json_schema_compatibility_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe JsonSchemaCompatibility do
+  it "has a version number" do
+    expect(JsonSchemaCompatibility::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
